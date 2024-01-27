fcb9adc24eae0528a845b04e2fb3f2dc2117a698
Raphael Lopes <ph.campanario07@gmail.com>
2024-01-27 14:40:01 -0300

    add nvim setup

--- tools/install.sh
+++ tools/install.sh
@@ -70,6 +70,7 @@ main() {
   # Install Plug.vim
   # env curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || {
   env curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || {
+
     printf "${RED}Error: Plug.vim install failed${NORMAL}\n\n"
     exit 1
   }
