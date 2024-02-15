be2be5ec3b118a550d8a3d1cdef10ad915930697
Raphael Lopes <ph.campanario07@gmail.com>
2024-02-15 15:23:32 -0300

    enable formatting on save

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -2,12 +2,28 @@ return {
   "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.eslint_d,
       }
+
+      -- formatting on save
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+          })
+        end
+      end,
     })
   end,
 }
