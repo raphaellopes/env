382cefad8e793246f08a0c5da597acf0864fcf12
Raphael Lopes <ph.campanario07@gmail.com>
2024-02-15 16:47:49 -0300

    add completions

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -16,9 +16,15 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
